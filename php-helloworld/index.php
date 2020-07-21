a81bb074452ce22e15eaf2dfbdcc169d979c8e95
Student User <student@workstation.lab.example.com>
2020-07-21 07:32:20 +0000

    add table

--- php-helloworld/index.php
+++ php-helloworld/index.php
@@ -1,4 +1,13 @@
 <?php
 print "Hello, World! php version is " . PHP_VERSION . "\n";
 print "\n Sarwo was here too";
+<div>
+ <table> 
+  <th>id </th>
+  <th>name </th>
+  <tr><td>1</td></tr>
+ </table>
+  
+</div>
+
 ?>
